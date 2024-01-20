354b964d64b1d981481760aab668d929a154a7e2
muhammad <kurbanovmagomed444@gmail.com>
2024-01-21 01:29:05 +0300

    First commit

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -11,13 +11,11 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 abstract class AbstractTestCase extends TestCase
 {
-    protected function setEntityId(object $entity, int $value, $idField = 'id')
+    protected function setEntityId(object $entity, int $value, $idField = 'id'): void
     {
         $class = new \ReflectionClass($entity);
         $property = $class->getProperty($idField);
-        $property->setAccessible(true);
         $property->setValue($entity, $value);
-        $property->setAccessible(false);
     }
 
     protected function assertResponse(int $expectedStatusCode, string $expectedBody, Response $actualResponse): void
